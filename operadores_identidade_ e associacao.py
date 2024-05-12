26337313c9fd5273661c26733cfdd2e36e7285be
Isabela Moura <80081603+isabelamoura@users.noreply.github.com>
2024-05-11 22:35:58 -0300

    operadores de identidade e associaçaõ

---
+++ operadores_identidade_ e associacao.py
@@ -0,0 +1,15 @@
+saldo = 1000
+limite = 600
+print(saldo is limite)
+print(saldo is not limite)
+
+#operadores_de_associação
+#ser sempre o mesmo nome, porque letra maiuscula ou minuscula pode alterar o resultado
+
+
+frutas= ["limao", "uva"]
+curso= "curso de python"
+
+print("laranja" not in frutas)
+print("limao"  in frutas)
+print("python" in curso)
