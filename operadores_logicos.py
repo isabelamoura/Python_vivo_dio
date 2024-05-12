007ad325aba66de217c02eda3a1f7e3f9f09caa3
Isabela Moura <80081603+isabelamoura@users.noreply.github.com>
2024-05-11 22:19:50 -0300

    operadores lógicos

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -1,5 +1,5 @@
-#AND = para ser TRUE tudo tem que ser True
-#OR= para ser True apenas um tem que ser True
+# AND = para ser TRUE tudo tem que ser True
+# OR= para ser True apenas um tem que ser True
 
 
 print(True and True and True)
@@ -12,17 +12,18 @@ print(False or False)
 saldo = 1000
 saque = 250
 limite = 200
-conta_especial= True
+conta_especial = True
 
 
-exp= saldo >= saque and saque <= limite or conta_especial and saldo >= saque
+exp = saldo >= saque and saque <= limite or conta_especial and saldo >= saque
 print(exp)
 
-exp_2= (saldo >=saque and saque <= limite) or (conta_especial and saldo >= saque)
+exp_2 = (saldo >= saque and saque <= limite) or (
+    conta_especial and saldo >= saque)
 print(exp_2)
 
-conta_normal_com_saldo_suficiente =(saldo >= saque and saque <= limite)
+conta_normal_com_saldo_suficiente = (saldo >= saque and saque <= limite)
 contae_especial_com_saldo_suficiente = (conta_especial and saldo >= saque)
 
-exp_3= conta_normal_com_saldo_suficiente or contae_especial_com_saldo_suficiente
+exp_3 = conta_normal_com_saldo_suficiente or contae_especial_com_saldo_suficiente
 print(exp_3)
