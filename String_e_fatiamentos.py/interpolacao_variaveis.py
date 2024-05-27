70f4b2c144535c038814adbd6c6bcf1bd16079cf
Isabela Moura <80081603+isabelamoura@users.noreply.github.com>
2024-05-27 14:50:34 -0300

    string e fatiamentos

---
+++ String_e_fatiamentos.py/interpolacao_variaveis.py
@@ -0,0 +1,8 @@
+nome= "Isabela"
+idade= 25
+profissao= "Programador"
+linguagem= "Python"
+
+print("Olá meu nome é %s. Eu tenho %d de idade, trabalho como %s, e estou matriculado em %s." % (nome, idade, profissao, linguagem))
+
+print("Ola meu nome é {}. Eu tenho {} anos de idade, trabalho como {} e estou matriculado  no curso de {}." . format(nome, idade, profissao, linguagem))
